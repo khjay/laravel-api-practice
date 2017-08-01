602b05361546af4ab79697bffa3b4ca84119380f
SJZeng <sjzeng@program.com.tw>
2017-08-02 02:01:45 +0800

    create a way to generate dummy data

---
+++ database/seeds/LessonsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+use App\Lesson;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+
+class LessonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach(range(1, 30) as $idx) {
+            Lesson::create([
+                'title' => $faker->sentence(5),
+                'body' => $faker->paragraph(4),
+                'some_bool' => $faker->boolean(),
+            ]);
+        }
+    }
+}
