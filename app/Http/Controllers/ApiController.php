6a9a6837e9952520db385586332978c80c753063
SJZeng <sjzeng@program.com.tw>
2017-08-02 23:23:58 +0800

    add more response

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Response;
+use Illuminate\Http\Response as IlluminateResponse;
 
 class ApiController extends Controller
 {
-    protected $statusCode = 200;
+    protected $statusCode = IlluminateResponse::HTTP_OK;
 
     public function getStatusCode()
     {
@@ -22,7 +23,19 @@ class ApiController extends Controller
 
     public function respondNotFound($message = 'Not Found!')
     {
-        return $this->setStatusCode(404)->respondWithError($message);
+        return $this->setStatusCode(IlluminateResponse::HTTP_NOT_FOUND)->respondWithError($message);
+    }
+
+    public function respondInternalError($message = 'Internal Error!')
+    {
+        return $this->setStatusCode(IlluminateResponse::HTTP_INTERNAL_SERVER_ERROR)->respondWithError($message);
+    }
+
+    public function respondCreated($message = 'Successfully Created.')
+    {
+        return $this->setStatusCode(IlluminateResponse::HTTP_CREATED)->respond([
+            'message' => $message
+        ]);
     }
 
     public function respond($data, $headers = [])
@@ -38,7 +51,5 @@ class ApiController extends Controller
                 'status_code' => $this->getStatusCode()
             ]
         ]);
-
-
     }
 }
